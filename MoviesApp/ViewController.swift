c78b05dcafd2a083eaaf221b5bdf9a7072a3b654
Burakguzey <burakguzey20@gmail.com>
2022-07-01 22:30:47 +0300

    Initial Commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Burak Güzey on 1.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
